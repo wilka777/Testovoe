6685aa89591680b68a98480da30e61d22394bb81
wilka <serega2522@mail.ru>
2023-07-10 18:26:39 +0500

    testovoe

--- local/templates/delement/components/bitrix/news.detail/BlogDetail/template.php
+++ local/templates/delement/components/bitrix/news.detail/BlogDetail/template.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -12,206 +12,204 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 $APPLICATION->SetPageProperty("PAGE_CLASS", 'page-article page-article--new');
+$resElement = CIBlockElement::GetByID($arResult['ID'])->GetNext();
+
 ?>
 
-<section class="section page--post page--post-new">
-    <div class="section__body page-article__wrap">
+    <section class="section page--post page--post-new">
+        <div class="section__body page-article__wrap">
 
-        <article class="post-layout tile user-free-content">
+            <article class="post-layout tile user-free-content">
 
-            <div class="post-layout__wrap">
-                <header class="post-layout__header article-summary">
-                    <ul class="post-layout__header-item article-summary__hashtag-list">
-                        <li class="article-summary__hashtag-item">
-                            <a href="#" class="article-summary__hashtag-link"><?= $arResult['PROPERTIES']['teg']['VALUE']?></a>
-                        </li>
-                    </ul>
-                    <div class="article-summary__post-info">
+                <div class="post-layout__wrap">
+                    <header class="post-layout__header article-summary">
+                        <ul class="post-layout__header-item article-summary__hashtag-list">
+                            <? foreach ($arResult['PROPERTIES']['teg']['VALUE'] as $arTegs) { ?>
+                                <li class="article-summary__hashtag-item">
+                                    <a href="#" class="article-summary__hashtag-link"><?= $arTegs ?></a>
+                                </li>
+                            <? } ?>
+                        </ul>
+                        <div class="article-summary__post-info">
                     <span class="article-summary__author">
-                        <?= $arResult['PROPERTIES']['author']['VALUE']?>
+                        <?= $arResult['PROPERTIES']['author']['VALUE'] ?>
                     </span>
-                        <time datetime="2019-05-28" class="post-layout__header-item post-layout__time article-summary__info-text">
-                            <?= strstr($arResult['DATE_ACTIVE_FROM'], ' ', true);?>
-                        </time>
-                    </div>
-                    <span class="post-layout__header-item post-layout__time-to-read article-summary__info-text">
-                  7 минут на чтение <!-- Как определяется время на чтение? -->
+                            <time datetime="2019-05-28"
+                                  class="post-layout__header-item post-layout__time article-summary__info-text">
+                                <?= strstr($arResult['DATE_ACTIVE_FROM'], ' ', true); ?>
+                            </time>
+                        </div>
+                        <span class="post-layout__header-item post-layout__time-to-read article-summary__info-text">
+                  <?= $arResult['PROPERTIES']['ReadingTime']['VALUE'] ?> на чтение
+                            <!-- Как определяется время на чтение -->
                 </span>
-                    <div class="pull-right article-summary__info-text article-summary__info-text--with-icon">
-                        50 <!-- Реализовать просмотры -->
-                        <span class="article-summary__info-icon article-summary__info-icon--eye">
+                        <div class="pull-right article-summary__info-text article-summary__info-text--with-icon">
+                            <?= $resElement["SHOW_COUNTER"]; ?> <!-- Реализовать просмотры -->
+                            <span class="article-summary__info-icon article-summary__info-icon--eye">
                         <svg width="22" height="22">
                           <use href="#icon-eye-3"></use>
                         </svg>
                       </span>
+                        </div>
+                    </header>
+
+                    <div class="post-layout__content">
+                        <!-- Заголовок Статьи -->
+                        <?= $arResult['PROPERTIES']['detail_taitle']['~VALUE']['TEXT']; ?>
+                        <!-- Заголовок Статьи -->
+
+                        <!-- Основной контент на странице-->
+                        <?= $arResult['DETAIL_TEXT']; ?>
+                        <!-- Основной контент на странице-->
+
+                        <!-- Слайдер -->
+                        <div class="post-layout__slider blog-gallery">
+                            <div class="swiper-container">
+                                <div class="swiper-wrapper" data-js-gallery-container>
+                                    <?php foreach ($arResult['PROPERTIES']['photos'] as $photo) { ?>
+                                        <div class="swiper-slide">
+                                            <img src="<?= $photo['src'] ?>"
+                                                 alt="" class="blog-gallery__img" data-js-zoom>
+                                        </div>
+                                    <?php } ?>
+
+
+                                </div>
+
+                                <div class="blog-gallery__navigation">
+                                    <button class="blog-gallery__btn blog-gallery__btn--prev slider-btn-rounded-blue">
+                                        <svg width="13" height="16" class="i-icon">
+                                            <use xlink:href="#icon-arrow--left"></use>
+                                        </svg>
+                                    </button>
+                                    <button class="blog-gallery__btn blog-gallery__btn--next slider-btn-rounded-blue">
+                                        <svg width="13" height="16" class="i-icon">
+                                            <use xlink:href="#icon-arrow--right"></use>
+                                        </svg>
+                                    </button>
+                                </div>
+
+                                <div class="blog-gallery__pagination"></div>
+
+                            </div>
+                        </div>
+
+                        <!-- Слайдер -->
+
+
                     </div>
-                </header>
 
-                <div class="post-layout__content">
+                    <footer class="post-layout__footer">
+                        <div class="blog-bottom">
+                            <div class="blog-bottom__aux">
+                                <a class="back-list-article link-with-arrow link-with-arrow--back"
+                                   href="<?= $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["news"] ?>">Вернуться
+                                    к
+                                    списку
+                                    статей</a>
+                            </div>
+                        </div>
+                    </footer>
 
-                    <!-- Основной контент на странице-->
+                </div>
 
-                    <h2>Что такое гостевой доступ?</h2>
-                    <p>
-                        Гостевой доступ в Яндекс.Директ и Google.Adwords — это возможность предоставить
-                        доступ к рекламной кампании, при этом не давая доступ к аккаунту, на котором была
-                        заведена рекламная кампания.
-                        Таким образом, подрядчик может получить уже полностью новый аккаунт, на котором есть
-                        доступ к редактированию/просмотру рекламной кампании.
-                    </p>
-                    <h2>Что дает гостевой доступ</h2>
-                    <p>Функционал гостевого доступа позволяет:</p>
+            </article>
 
-                    <ul>
-                        <li>Войдите в свою учетную запись Яндекс Директ;
+            <aside class="page-article__aside">
+                <div class="topics">
+                    Читайте также
+                    <div class="topics__content">
+
+                        <div class="topics__tab">
+
+                            <ul class="topics__tab-list">
+                                <li class="topics__tab-item">
+                                    <div class="topics__tab-item-top">
+                                        <a class="link" href="#" target="_blank">Интернет-маркетинг</a>
+                                        <time>30.янв.2022</time>
+                                    </div>
+
+                                    <a class="topics__tab-item-title only-2-lines" href="#" target="_blank">Как
+                                        бесплатно
+                                        продвинуть мобильное приложение</a>
+                                </li>
+                                <li class="topics__tab-item">
+                                    <div class="topics__tab-item-top">
+                                        <a class="link" href="#" target="_blank">Интернет-маркетинг</a>
+                                        <time>30.янв.2022</time>
+                                    </div>
+
+                                    <a class="topics__tab-item-title only-2-lines" href="#" target="_blank">Отличия
+                                        контекстной
+                                        и таргетированной рекламы</a>
+                                </li>
+                                <li class="topics__tab-item">
+                                    <div class="topics__tab-item-top">
+                                        <a class="link" href="#" target="_blank">Интернет-маркетинг</a>
+                                        <time>30.янв.2022</time>
+                                    </div>
+
+                                    <a class="topics__tab-item-title only-2-lines" href="#" target="_blank">Как
+                                        бесплатно
+                                        продвинуть мобильное приложение</a>
+                                </li>
+                            </ul>
+
+                            <a class="link-with-arrow" href="#" target="_blank">Все статьи</a>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="promotions promotions--sticky-position">
+                    <a class="promotions__link" href="" target="_blank">Выгодные предложения и акции</a>
+
+                    <ul class="promotions__list">
+                        <li class="promotions__item">
+                            <div class="promotions__item-img">
+                                <img src="/local/templates/delement/frontend/assets/images/post/promotion.jpg"
+                                     width="146"
+                                     height="140" alt="">
+                            </div>
+                            <a class="promotions__item-link" href="">Круглый год! Установка и год обслуживания
+                                SSL-сертификат в
+                                подарок</a>
                         </li>
-                        <li>Установить или отменить настройки (редактировать любые настройки рекламы);
+
+                        <li class="promotions__item">
+                            <div class="promotions__item-img">
+                                <img src="/local/templates/delement/frontend/assets/images/post/promotion.jpg"
+                                     width="146"
+                                     height="140" alt="">
+                            </div>
+                            <a class="promotions__item-link" href="">Круглый год! Установка и год обслуживания
+                                SSL-сертификат в
+                                подарок</a>
                         </li>
-                        <li>Оплачивать рекламу (пополнять бюджет рекламы).</li>
+
+                        <li class="promotions__item">
+                            <div class="promotions__item-img">
+                                <img src="/local/templates/delement/frontend/assets/images/post/promotion.jpg"
+                                     width="146"
+                                     height="140" alt="">
+                            </div>
+                            <a class="promotions__item-link" href="">Круглый год! Установка и год обслуживания
+                                SSL-сертификат в
+                                подарок</a>
+                        </li>
                     </ul>
 
-                    <p>
-                        Давайте рассмотрим интерфейс Яндекс.Директ и Google.Adwords изнутри и узнаем как
-                        выдать гостевой доступ.
-                    </p>
-
-                    <h2>Как выдать гостевой доступ в Яндекс.Директ</h2>
-                    <p>Первоначально у Вас должна быть уже создана рекламная кампания, к которой вы хотите
-                        предоставить гостевой доступ.</p>
-
-                    <p class="outlined">В свое время Sketch стал революционным продуктом, что послужило
-                        отличной почвой для создания еще более высококлассных программ.</p>
-
-                    <!-- Основной контент на странице-->
-
-                    <!-- Слайдер -->
-                    <div class="post-layout__slider blog-gallery">
-                        <div class="swiper-container">
-                            <div class="swiper-wrapper" data-js-gallery-container>
-                                <?php foreach ($arResult['PROPERTIES']['photos'] as $photo) { ?>
-                                    <div class="swiper-slide">
-                                        <img src="<?= $photo['src'] ?>"
-                                             alt="" class="blog-gallery__img" data-js-zoom>
-                                    </div>
-                                <?php } ?>
-
-
-                            </div>
-
-                            <!-- Слайдер -->
-
-                            <div class="blog-gallery__navigation">
-                                <button class="blog-gallery__btn blog-gallery__btn--prev slider-btn-rounded-blue">
-                                    <svg width="13" height="16" class="i-icon">
-                                        <use xlink:href="#icon-arrow--left"></use>
-                                    </svg>
-                                </button>
-                                <button class="blog-gallery__btn blog-gallery__btn--next slider-btn-rounded-blue">
-                                    <svg width="13" height="16" class="i-icon">
-                                        <use xlink:href="#icon-arrow--right"></use>
-                                    </svg>
-                                </button>
-                            </div>
-
-                            <div class="blog-gallery__pagination"></div>
-
-                        </div>
-                    </div>
-
-                    <!-- Слайдер -->
-
-
                 </div>
+            </aside>
 
-                <footer class="post-layout__footer">
-                    <div class="blog-bottom">
-                        <div class="blog-bottom__aux">
-                            <a class="back-list-article link-with-arrow link-with-arrow--back" href="#">Вернуться
-                                к
-                                списку
-                                статей</a>
-                        </div>
-                    </div>
-                </footer>
+        </div>
 
-            </div>
+    </section>
 
-        </article>
-
-        <aside class="page-article__aside">
-            <div class="topics">
-                Читайте также
-                <div class="topics__content">
-
-                    <div class="topics__tab">
-
-                        <ul class="topics__tab-list">
-                            <li class="topics__tab-item">
-                                <div class="topics__tab-item-top">
-                                    <a class="link" href="#" target="_blank">Интернет-маркетинг</a>
-                                    <time>30.янв.2022</time>
-                                </div>
-
-                                <a class="topics__tab-item-title only-2-lines" href="#" target="_blank">Как бесплатно продвинуть мобильное приложение</a>
-                            </li>
-                            <li class="topics__tab-item">
-                                <div class="topics__tab-item-top">
-                                    <a class="link" href="#" target="_blank">Интернет-маркетинг</a>
-                                    <time>30.янв.2022</time>
-                                </div>
-
-                                <a class="topics__tab-item-title only-2-lines" href="#" target="_blank">Отличия контекстной и таргетированной рекламы</a>
-                            </li>
-                            <li class="topics__tab-item">
-                                <div class="topics__tab-item-top">
-                                    <a class="link" href="#" target="_blank">Интернет-маркетинг</a>
-                                    <time>30.янв.2022</time>
-                                </div>
-
-                                <a class="topics__tab-item-title only-2-lines" href="#" target="_blank">Как бесплатно продвинуть мобильное приложение</a>
-                            </li>
-                        </ul>
-
-                        <a class="link-with-arrow" href="#" target="_blank">Все статьи</a>
-                    </div>
-
-                </div>
-            </div>
-
-            <div class="promotions promotions--sticky-position">
-                <a class="promotions__link" href="" target="_blank">Выгодные предложения и акции</a>
-
-                <ul class="promotions__list">
-                    <li class="promotions__item">
-                        <div class="promotions__item-img">
-                            <img src="/local/templates/delement/frontend/assets/images/post/promotion.jpg" width="146" height="140" alt="">
-                        </div>
-                        <a class="promotions__item-link" href="">Круглый год! Установка и год обслуживания SSL-сертификат в подарок</a>
-                    </li>
-
-                    <li class="promotions__item">
-                        <div class="promotions__item-img">
-                            <img src="/local/templates/delement/frontend/assets/images/post/promotion.jpg" width="146" height="140" alt="">
-                        </div>
-                        <a class="promotions__item-link" href="">Круглый год! Установка и год обслуживания SSL-сертификат в подарок</a>
-                    </li>
-
-                    <li class="promotions__item">
-                        <div class="promotions__item-img">
-                            <img src="/local/templates/delement/frontend/assets/images/post/promotion.jpg" width="146" height="140" alt="">
-                        </div>
-                        <a class="promotions__item-link" href="">Круглый год! Установка и год обслуживания SSL-сертификат в подарок</a>
-                    </li>
-                </ul>
-
-            </div>
-        </aside>
-
-    </div>
-
-</section>
 <?php
+
 echo '<pre>';
-echo print_r($arResult['PROPERTIES']);
-echo '<pre>';
+//print_r($arResult);
+//echo print_r($arResult['DETAIL_TEXT']);
+echo '</pre>';
 ?>
--- local/templates/delement/components/bitrix/news.list/BlogElementsList/template.php
+++ local/templates/delement/components/bitrix/news.list/BlogElementsList/template.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -11,62 +11,80 @@
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
+
+
 ?>
 
 
-<div class=" page-blog__cards">
-    <?php foreach ($arResult['ITEMS'] as $arItem) { ?>
-    <article class="card-post card-post--new card-post--full ">
-        <div class="card-post__img">
-            <img src="<?=$arItem['PREVIEW_PICTURE']['SRC'];?>"
-                 alt="Как предоставить доступ к Яндекс Директ и Google Adwords">
-        </div>
-        <div class="card-post__info ">
-            <div class="card-post__summary article-summary">
-                <ul class="article-summary__hashtag-list">
-                    <li class="article-summary__hashtag-item">
-                        <a href="#" class="article-summary__hashtag-link"><?=$arItem['PROPERTIES']['teg']['VALUE'];?></a>
-                    </li>
-                </ul>
-            </div>
-            <a href="<?=$arItem['DETAIL_PAGE_URL'];?>" class="card-post__link">
-                <h3 class="card-post__title"><?=$arItem['NAME'];?></h3>
-            </a>
-            <div class="card-post__preview-text">
-                <p>
-                    <?=$arItem['PREVIEW_TEXT']; // ДОДЕЛАТЬ ЕСЛИ ОТСУТСТВУЕТ?>
-                </p>
-            </div>
-            <div class="card-post__bottom article-summary">
-                <div class="article-summary__info-block">
-                  <span class="article-summary__author">
-                    <?=$arItem['PROPERTIES']['author']['VALUE'];?>
-                  </span>
-                    <time datetime="2022-01-28" class="article-summary__info-text">
-                        <?= strstr($arItem['DATE_ACTIVE_FROM'], ' ', true);?>
-                    </time>
+    <div class=" page-blog__cards">
+        <?php foreach ($arResult['ITEMS'] as $arItem) { ?>
+            <article class="card-post card-post--new card-post--full ">
+                <div class="card-post__img">
+                    <img src="<?= $arItem['PREVIEW_PICTURE']['SRC']; ?>"
+                         alt="Как предоставить доступ к Яндекс Директ и Google Adwords">
                 </div>
-                <div class="pull-right article-summary__info-text article-summary__info-text--with-icon">
-                    <? echo 50 // Вывод кол-во просмотров, реализовать позже?>
-                    <span class="article-summary__info-icon article-summary__info-icon--eye">
+                <div class="card-post__info ">
+                    <div class="card-post__summary article-summary">
+                        <ul class="article-summary__hashtag-list">
+                            <? foreach ($arItem['PROPERTIES']['teg']['VALUE'] as $arTegs) { ?>
+                                <li class="article-summary__hashtag-item">
+                                    <a href="#" class="article-summary__hashtag-link"><?= $arTegs ?></a>
+                                </li>
+                            <? } ?>
+                        </ul>
+                    </div>
+                    <a href="<?= $arItem['DETAIL_PAGE_URL']; ?>" class="card-post__link">
+                        <h3 class="card-post__title"><?= $arItem['NAME']; ?></h3>
+                    </a>
+                    <div class="card-post__preview-text">
+                        <p>
+                            <? if (empty($arItem['PREVIEW_TEXT'])) {
+                                $newStr = strip_tags($arItem['DETAIL_TEXT']);
+                                $length = $arItem['PROPERTIES']['text_previw_limit']['VALUE'];
+                                if (empty($length)) {
+                                    $length = 15;
+                                }
+                                $newPreview = explode(' ', $newStr);
+                                $Out = '';
+                                foreach ($newPreview as $key => $value) {
+                                    if ($key >= $length) {
+                                        break;
+                                    }
+                                    $Out .= $value . ' ';
+                                }
+                                echo $Out;
+                            } else { echo $arItem['PREVIEW_TEXT']; }
+
+                            ?>
+
+                        </p>
+                    </div>
+                    <div class="card-post__bottom article-summary">
+                        <div class="article-summary__info-block">
+                  <span class="article-summary__author">
+                    <?= $arItem['PROPERTIES']['author']['VALUE']; ?>
+                  </span>
+                            <time datetime="" class="article-summary__info-text">
+                                <?= strstr($arItem['DATE_ACTIVE_FROM'], ' ', true); ?>
+                            </time>
+                        </div>
+                        <div class="pull-right article-summary__info-text article-summary__info-text--with-icon">
+                            <?= $arItem['SHOW_COUNTER'] ?>
+                            <span class="article-summary__info-icon article-summary__info-icon--eye">
                     <svg width="22" height="22">
                       <use href="#icon-eye-3"></use>
                     </svg>
                   </span>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </article>
+            </article>
 
-    <?php } ?>
+        <?php } ?>
 
-</div>
-<??>
-    <br />
+    </div>
+
+    <br/>
 
 
-<?=$arResult["NAV_STRING"]?>
-
-
-
-
+<?= $arResult["NAV_STRING"] ?>
